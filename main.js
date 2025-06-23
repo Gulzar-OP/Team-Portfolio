7f029e5375b68708fa9875aef9a2113062de6932
Gulzar Hussain <gulzarhu88@gmail.com>
2025-06-23 06:59:50 +0530

    add files

---
+++ main.js
@@ -0,0 +1,9 @@
+const lenis = new lenis()
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        requestAnimationFrame(raf)
+
+
+        // 
